1c6fcf37c7e5bed5ec7b36b09aadf26e942aa59f
Duc Thanh Nguyen <ngtduc693@gmail.com>
2024-11-28 10:43:05 +0700

    init the project

---
+++ SupersetClientBase/Interfaces/ISupersetClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupersetClientBase.Interfaces
+{
+    internal interface ISupersetClient
+    {
+        Task<string> GetDashboardsAsync();
+        Task<string> GetChartsAsync();
+        Task<string> ExecuteSqlAsync(string query);
+    }
+}
---
+++ SupersetClientBase/SupersetClient.cs
@@ -0,0 +1,46 @@
+﻿using SupersetClientBase.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupersetClientBase
+{
+    internal class SupersetClient : ISupersetClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public SupersetClient(string baseUrl, string accessToken)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        public async Task<string> GetDashboardsAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/v1/dashboard/");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetChartsAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/v1/chart/");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> ExecuteSqlAsync(string query)
+        {
+            var content = new StringContent($"{{\"query\": \"{query}\"}}", Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("/api/v1/sqllab/", content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
